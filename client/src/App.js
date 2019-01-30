87058d406aa4c227d2e7b69992c58eb2e7a1b328
Saba Schock <sababorjsaba@gmail.com>
2019-01-29 21:21:07 -0800

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import './App.css';
-import NoMatch from "./pages/Nomatch";
 import Save from "./pages/Save";
 import Search from "./pages/Search"
 import Nav from "./components/Nav";
@@ -15,7 +20,6 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Search}/>
             <Route exact path="/save" component={Save} />
-            <Route component={NoMatch} />
           </Switch>
         </div>
       </Router>
--- client/src/pages/Save.js
+++ client/src/pages/Save.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 
 class Save extends Component {
   render() {
