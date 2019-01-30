7799e1e03c594fca637fc865dc6a3f8cba754cb1
Saba Schock <sababorjsaba@gmail.com>
2019-01-29 21:08:56 -0800

    cliend draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import NoMatch from "./pages/Nomatch";
+import Save from "./pages/Save";
+import Search from "./pages/Search"
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
-    return (<>
-    </>)
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Search}/>
+            <Route exact path="/save" component={Save} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 export default App;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+
+class Search extends Component {
+  render() {
+    return (<div>
+        Search
+    </div>)
+}
+}
+
+export default Search;
